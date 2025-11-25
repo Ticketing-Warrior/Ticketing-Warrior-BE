6d54c7b7d5dc25c7f8c60b6385c9558cfed2113b
wldsmdl7 <wldmsdl5835@gmail.com>
2025-11-25 14:06:59 +0900

    chore : 프로젝트 세팅

---
+++ src/routes/routes.index.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const routers = Router();
+
+// routers.use("/api/user", userRouter);
+
+export default routers;
---
+++ src/routes/user.routers.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+// user 도메인 라우터
+// router.get("/login", handleLogin);
+
+export default router;
