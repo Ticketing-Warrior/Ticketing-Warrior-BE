6f879f1b8f732ca1074d19c766dca8438561fc7e
wldsmdl7 <wldmsdl5835@gmail.com>
2025-11-26 16:51:16 +0900

    [#1] fix : 티켓팅 시작 API 예외처리 구현

--- src/controllers/ticketingController.js
+++ src/controllers/ticketingController.js
@@ -1,9 +1,14 @@
+import { BadRequestError } from '../errors.js';
 import { successHandler } from '../middlewares/responseHandler.js';
 import { startTicketing } from '../services/ticketingService.js';
 
 export const handleStartTicketing = async (req, res, next) => {
     try {
-        const nickname = req.body.nickname;
+        const nickname = req.body?.nickname;
+
+        if(!nickname || nickname === undefined){
+            throw new BadRequestError("닉네임을 확인 후, 다시 이용해주세요.");
+        }
 
         const resposne = await startTicketing(nickname);
         return successHandler(res, `티켓팅 시작`, resposne )
