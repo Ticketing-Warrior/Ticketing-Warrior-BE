b040786d88ddfece4b2267054839b3fe441f64fa
wldsmdl7 <wldmsdl5835@gmail.com>
2025-11-28 21:36:28 +0900

    feat : 좌석 상태 변경 API 구현

--- src/routes/seat.router.js
+++ src/routes/seat.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { handleSingleSeat } from "../controllers/seat.controller.js";
+import { handleSingleSeat, handleChangeSeatState } from "../controllers/seat.controller.js";
 
 const router = Router();
 
 // 좌석 단일 조회
 router.get("/single", handleSingleSeat);
+router.patch("/change", handleChangeSeatState);
 
 export default router;
--- src/services/seat.service.js
+++ src/services/seat.service.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { redisClient } from '../redis/redisClient.js';
-import { NotFoundError } from '../errors.js';
+import { ExistsError, NotFoundError } from '../errors.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,4 +22,37 @@ export async function getSingleSeat(seatId) {
   }
 
   return result;
+}
+
+export async function changeSeatState (seatId, state) {
+  console.log(seatId, state);
+  console.log(`seatId = ${seatId}`, typeof seatId);
+  const isExist = await runSeatScript(['seats'], ['getSingleSeat', seatId]);
+
+  console.log(isExist);
+  if(!isExist){
+    throw new NotFoundError('존재하지 않는 좌석입니다.');
+  }
+
+  let result = null;
+  if(state === "lock"){
+     result = await runSeatScript(['seats'], ['lockSeat', seatId]);
+
+    if(!result){
+      throw new ExistsError("이미 판매된 좌석입니다.");
+    }
+
+  }else if(state === "available"){
+    result = await runSeatScript(['seats'], ['ableSeat', seatId]);
+
+    if(!result){
+      throw new ExistsError("이미 선택 해제된 좌석입니다.");
+    }
+
+  }else{
+    throw new NotFoundError("존재하지 않는 상태값입니다.");
+  }
+
+  return { seatId, state}
+
 }
