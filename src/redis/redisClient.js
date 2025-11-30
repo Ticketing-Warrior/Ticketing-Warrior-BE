e319f50f388b8be0669469acb847c6c32aeed87e
Zuzzang2 <mandy021021@gmail.com>
2025-11-30 16:05:11 +0900

    feat: 프로덕션 환경용 조건부 TLS 연결 지원 추가
    ElastiCache 암호화 통신을 위해 NODE_ENV가 production일 때만 Redis 클라이언트의 TLS 옵션을 활성화하도록 설정.

--- src/redis/redisClient.js
+++ src/redis/redisClient.js
@@ -1,11 +1,14 @@
 import Redis from "ioredis";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  // password: process.env.REDIS_PASSWORD || undefined, // 로컬에선 주석처리
+  // password: process.env.REDIS_PASSWORD || undefined, //로컬에서는 주석 처리
   maxRetriesPerRequest: 3, // 재시도 옵션 설정
-  // tls: {} // 로컬에선 주석처리
+	// TLS 설정 자동 처리
+  tls: isProduction ? {} : undefined
 });
 
 redisClient.on("connect", () => {
